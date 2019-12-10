865d0331e1f34dfdd5bd4665acb8af2e64ecd20b
Manoj Dhadke <manoj.dhadke@getflint.io>
2019-12-10 12:38:30 +0530

    Flint license header updated

--- flint/Wintel FlintBits/email_trigger.rb
+++ flint/Wintel FlintBits/email_trigger.rb
@@ -1,3 +1,20 @@
+=begin
+#
+#  INFIVERVE TECHNOLOGIES PTE LIMITED CONFIDENTIAL
+#  _______________________________________________
+# 
+#  (C) INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE
+#  All Rights Reserved.
+#  Product / Project: Flint IT Automation Platform
+#  NOTICE:  All information contained herein is, and remains
+#  the property of INFIVERVE TECHNOLOGIES PTE LIMITED.
+#  The intellectual and technical concepts contained
+#  herein are proprietary to INFIVERVE TECHNOLOGIES PTE LIMITED.
+#  Dissemination of this information or any form of reproduction of this material
+#  is strictly forbidden unless prior written permission is obtained
+#  from INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE.
+=end
+
 @log.info("STARTED execution of 'email_trigger.rb' script")
 from = @input.get("from")
 to = @input.get("to")
--- flint/Wintel FlintBits/hello1.rb
+++ flint/Wintel FlintBits/hello1.rb
@@ -1,3 +1,20 @@
+=begin
+#
+#  INFIVERVE TECHNOLOGIES PTE LIMITED CONFIDENTIAL
+#  _______________________________________________
+# 
+#  (C) INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE
+#  All Rights Reserved.
+#  Product / Project: Flint IT Automation Platform
+#  NOTICE:  All information contained herein is, and remains
+#  the property of INFIVERVE TECHNOLOGIES PTE LIMITED.
+#  The intellectual and technical concepts contained
+#  herein are proprietary to INFIVERVE TECHNOLOGIES PTE LIMITED.
+#  Dissemination of this information or any form of reproduction of this material
+#  is strictly forbidden unless prior written permission is obtained
+#  from INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE.
+=end
+
 # This is an first flintbit tutorial - Hello World!!
 
 @log.info("Welcome to flint!")
